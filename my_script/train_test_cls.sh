93a6004a2ad4ad3fba897faa2a168fd0af2bfe7f
Daniellli <18750910084@163.com>
2022-09-04 16:38:29 +0800

    modify cls train code

--- my_script/train_test_cls.sh
+++ my_script/train_test_cls.sh
@@ -12,22 +12,25 @@
 
 
 
-
-
 # * dataset could be [sr3d, nr3d, scanrefer, scannet, sr3d+]
 #!  NR3D and ScanRefer  need much more epoch for converge 
 #!  To train on multiple datasets, e.g. on SR3D and NR3D simultaneously, set --TRAIN_DATASET sr3d nr3d.
 
-train_data="sr3d nr3d scanrefer scannet sr3d+"
-test_data=scanrefer
+# train_data="sr3d nr3d scanrefer scannet sr3d+"
+
+train_data=nr3d
+test_data=nr3d
 DATA_ROOT=datasets/
-gpu_ids="2,3"
-gpu_num=2
-# b_size=10
-b_size=1
+# gpu_ids="0,1,2,3,4,5,6,7"
+# gpu_num=8
+
+gpu_ids="0,4,6,7"
+gpu_num=4
+
+b_size=4
 port=29522
 # TORCH_DISTRIBUTED_DEBUG=INFO CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node 1 --master_port $RANDOM \
-TORCH_DISTRIBUTED_DEBUG=INFO python -m torch.distributed.launch --nproc_per_node $gpu_num --master_port $port \
+TORCH_DISTRIBUTED_DEBUG=INFO CUDA_VISIBLE_DEVICES=$gpu_ids python -m torch.distributed.launch --nproc_per_node $gpu_num --master_port $port \
     train_dist_mod.py --num_decoder_layers 6 \
     --use_color \
     --weight_decay 0.0005 \
@@ -41,6 +44,7 @@ TORCH_DISTRIBUTED_DEBUG=INFO python -m torch.distributed.launch --nproc_per_node
     --lr_decay_epochs 25 26 \
     --pp_checkpoint $DATA_ROOT/gf_detector_l6o256.pth \
     --butd_cls --self_attend \
-    --gpu-ids $gpu_ids \
-    --max_epoch 5 \
+    --max_epoch 150 \
     2>&1 | tee -a logs/train_test_cls.log
+# --upload-wandb \
+# --checkpoint_path $resume_mode_path \
