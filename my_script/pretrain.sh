97b2856dafe2a23e45b6058641a080e0543d7670
Daniellli <18750910084@163.com>
2022-10-24 17:02:18 +0800

    add resume lr decay code

--- my_script/pretrain.sh
+++ my_script/pretrain.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: xushaocong
  # @Date: 2022-10-24 00:27:51
- # @LastEditTime: 2022-10-24 00:34:59
+ # @LastEditTime: 2022-10-24 16:34:16
  # @LastEditors: xushaocong
  # @Description: 
  # @FilePath: /butd_detr/my_script/pretrain.sh
@@ -36,12 +36,12 @@ b_size=12
 
 
 port=29526
-save_freq=5;
-val_freq=5;
+save_freq=1;
+val_freq=1;
 print_freq=100;
 
 
-# resume_mode_path=pretrain/pretrain_ramdom%20anno_41.pth;
+resume_mode_path=logs/bdetr/scanrefer/1666543058/ckpt_epoch_100_best.pth;
 #* for  semi supervision architecture  : step1 
 # topk=8;
 labeled_ratio=0.3;
@@ -59,17 +59,20 @@ TORCH_DISTRIBUTED_DEBUG=INFO CUDA_VISIBLE_DEVICES=$gpu_ids python -m torch.distr
     --pp_checkpoint $DATA_ROOT/gf_detector_l6o256.pth \
     --butd_cls --self_attend \
     --max_epoch 400 \
+    --upload-wandb \
     --labeled_ratio $labeled_ratio \
+    --checkpoint_path $resume_mode_path \
+    --lr_decay_epochs 101 105 \
+    --lr_decay_intermediate \
     2>&1 | tee -a logs/train_test_cls.log
 
     
-# --upload-wandb \
+
 #  --joint_det
-# --checkpoint_path $resume_mode_path \
-#     --lr_decay_intermediate \
+
 # --use-tkps \
 # --query_points_obj_topk $topk \
-# --lr_decay_epochs 25 26 \
+
 # --dbeug \
 
 
